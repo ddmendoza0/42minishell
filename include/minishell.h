955259fc13b2da88a1f83b5e0054e6bd1e95bb02
Dani Maya <149480004+bossmaya6@users.noreply.github.com>
2025-07-10 15:48:17 +0200

    Add files via upload

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: dmendoza <dmendoza@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 18:36:45 by dmendoza          #+#    #+#             */
-/*   Updated: 2025/07/08 19:07:55 by dmendoza         ###   ########.fr       */
+/*   Updated: 2025/07/10 14:42:30 by dmaya-vi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@
 /*			LIBRARIES		 		*/
 /****************************************************************/
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 # include "libft.h"
-
+#include <fcntl.h>
 //for the readline function and functioning history
 #include <readline/readline.h>
 #include <readline/history.h>
@@ -29,6 +30,9 @@
 /*			END LIBRARIES				*/
 /****************************************************************/
 
+#define HISTORY_FILE ".minishell_history"
 
+
+void	write_to_history_file(char *input, int history_fd);
 
 #endif
