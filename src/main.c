e25e5b8862edf87bae9a33f8f6ef2405766a6ba4
David Mendoza Fernandez <dmendoza@car11s5.42barcelona.com>
2025-07-11 18:44:28 +0200

    skip empty lines and triming leading and trailing WHITESPACES, better logic of validations, better readability

--- src/main.c
+++ src/main.c
@@ -6,41 +6,55 @@
 /*   By: dmendoza <dmendoza@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 18:36:11 by dmendoza          #+#    #+#             */
-/*   Updated: 2025/07/10 17:00:53 by dmaya-vi         ###   ########.fr       */
+/*   Updated: 2025/07/11 18:41:59 by dmendoza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+
+static char	*trim_input(const char *input)
+{
+	if (!input)
+		return (NULL);
+	return (ft_strtrim(input, WHITESPACES));
+}
+
 static void input_loop(int history_fd)
 {
-	char *input;
+	char	*input;
+	char	*clean_input;
 
 	while (1)
 	{
 		input = readline("$minishell:");
-		
+		//VALIDATIONS
 		if (input == NULL)//Ctrl+D
 			break;
-		if(strcmp(input, "exit") == 0)
+		clean_input = trim_input(input);
+		if (!clean_input || *clean_input == '\0')//this skips empty line
+		{
+			free(input);
+			free(clean_input);
+			continue;
+		}
+		if(strcmp(clean_input, "exit") == 0)
 		{
 			//working history file logic
 			//write_history(HISTORY_FILE);
+			free(clean_input);
 			free(input);
 			break;
 		}
-		if (*input != '\0')//dont save empty
-		{
-			add_history(input);
-			write_to_history_file(input, history_fd);
-			if (strcmp(input, "history") == 0)
-				cmd_history();
-			//valid command checker here
-			//execute command, else throw error
-		}
-		//logic
-
-		ft_printf("%s\n", input);//test placeholder
+		//MAIN LOGIC
+		add_history(input);
+		write_to_history_file(input, history_fd);
+		if (strcmp(input, "history") == 0)
+			cmd_history();
+		//valid command checker here
+		//execute command, else throw error
+		ft_printf("%s\n", clean_input);//test placeholder
+		free(clean_input);
 		free(input);
 	}
 }
