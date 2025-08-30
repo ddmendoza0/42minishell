8eb8f08e0a5a79a614a6932c808904d748f4c400
ddmendoza0 <ddmendozafernandez@gmail.com>
2025-08-30 18:35:42 +0200

    norminette compliance - builtin_cd and builtin_utils

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -1,86 +1,66 @@
 #include "minishell.h"
 
-/*
- * UPDATE ENVIRONMENT VARIABLES
- * Updates PWD after successful directory change
- */
-static int update_pwd_var(t_shell *shell, char *new_pwd)
+static int	update_pwd_var(t_shell *shell, char *new_pwd)
 {
-    char    *pwd_var;
+	char	*pwd_var;
 
-    if (!shell || !new_pwd)
-        return (0);
-    // Create PWD=new_path string
-    pwd_var = malloc(strlen("PWD=") + strlen(new_pwd) + 1);
-    if (!pwd_var)
-        return (0);
-    sprintf(pwd_var, "PWD=%s", new_pwd);
-    // Update PWD in environment
-    if (!set_env_var(shell, pwd_var))
-    {
-        free(pwd_var);
-        return (0);
-    }
-    free(pwd_var);
-    // Update shell's cwd
-    if (shell->cwd)
-        free(shell->cwd);
-    shell->cwd = ft_strdup(new_pwd);
-    return (1);
+	if (!shell || !new_pwd)
+		return (0);
+	pwd_var = ft_strjoin("PWD=", new_pwd);
+	if (!pwd_var)
+		return (0);
+	if (!set_env_var(shell, pwd_var))
+	{
+		free(pwd_var);
+		return (0);
+	}
+	free(pwd_var);
+	if (shell->cwd)
+		free(shell->cwd);
+	shell->cwd = ft_strdup(new_pwd);
+	return (1);
 }
 
-/*
- * GET CURRENT WORKING DIRECTORY
- * Gets the current working directory after chdir
- */
-static char *get_current_directory(void)
+static char	*get_current_directory(void)
 {
-    return (getcwd(NULL, 0));
+	return (getcwd(NULL, 0));
 }
 
-/*
- * SIMPLIFIED CD BUILTIN FUNCTION
- * Only handles relative or absolute paths
- */
-int builtin_cd(char **argv, t_shell *shell)
+static int	validate_cd_args(char **argv, t_shell *shell)
 {
-    char    *target_path;
-    char    *new_pwd;
+	if (!argv[1])
+	{
+		print_error("minishell: cd", NULL, "missing argument");
+		return (set_exit_status(shell, EXIT_FAILURE));
+	}
+	if (argv[2])
+	{
+		print_error("minishell: cd", NULL, "too many arguments");
+		return (set_exit_status(shell, EXIT_FAILURE));
+	}
+	return (0);
+}
 
-    if (!shell)
-        return EXIT_FAILURE;
-    // Check if path argument is provided
-    if (!argv[1])
-    {
-        print_error("minishell: cd", NULL, "missing argument");
-        return set_exit_status(shell, EXIT_FAILURE);
-    }
-    // Check for too many arguments
-    if (argv[2])
-    {
-        print_error("minishell: cd", NULL, "too many arguments");
-        return set_exit_status(shell, EXIT_FAILURE);
-    }
-    target_path = argv[1];
-    // Actually change directory
-    if (chdir(target_path) != 0)
-    {
-        int error_type = errno;
-        return cd_error(shell, target_path, error_type);
-    }
-    // Get the actual current directory after chdir
-    new_pwd = get_current_directory();
-    if (!new_pwd)
-    {
-        return (handle_error(shell, ERR_MEMORY, "getting current directory"));
-    }
-    // Update PWD environment variable
-    if (!update_pwd_var(shell, new_pwd))
-    {
-        free(new_pwd);
-        return handle_error(shell, ERR_MEMORY, "updating PWD variable");
-    }
+int	builtin_cd(char **argv, t_shell *shell)
+{
+	char	*new_pwd;
+	int		validation_result;
 
-    free(new_pwd);
-    return (set_exit_status(shell, EXIT_SUCCESS));
+	if (!shell)
+		return (EXIT_FAILURE);
+	validation_result = validate_cd_args(argv, shell);
+	if (validation_result != 0)
+		return (validation_result);
+	if (chdir(argv[1]) != 0)
+		return (cd_error(shell, argv[1], errno));
+	new_pwd = get_current_directory();
+	if (!new_pwd)
+		return (handle_error(shell, ERR_MEMORY, "getting current directory"));
+	if (!update_pwd_var(shell, new_pwd))
+	{
+		free(new_pwd);
+		return (handle_error(shell, ERR_MEMORY, "updating PWD variable"));
+	}
+	free(new_pwd);
+	return (set_exit_status(shell, EXIT_SUCCESS));
 }
--- src/builtin_utils.c
+++ src/builtin_utils.c
@@ -1,79 +1,88 @@
-
 #include "minishell.h"
 
 /*
- * ENVIRONMENT VARIABLE SETTER
- * This function updates or adds a variable in shell->env
+ * FIND EXISTING VARIABLE INDEX
+ * Returns index of existing variable or -1 if not found
  */
-int set_env_var(t_shell* shell, char* var_assignment)
+static int	find_env_var(char **env, char *var_name, int var_name_len)
 {
-    char* var_name;
-    char* equals_pos;
-    int var_name_len;
-    int i;
-    int env_count;
-    char** new_env;
+	int	i;
 
-    if (!shell || !var_assignment)
-        return 0;
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], var_name, var_name_len) == 0 && env[i][var_name_len] == '=')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
 
-    equals_pos = ft_strchr(var_assignment, '=');
-    if (!equals_pos)
-        return 0;
+static int	replace_env_var(t_shell *shell, int index, char*var_assignment)
+{
+	free(shell->env[index]);
+	shell->env[index] = ft_strdup(var_assignment);
+	return (shell->env[index] != NULL);
+}
 
-    var_name_len = equals_pos - var_assignment;
-    var_name = ft_strndup(var_assignment, var_name_len);
-    if (!var_name)
-        return 0;
+static int	add_new_env_var(t_shell	*shell, char *var_assignment, int env_count)
+{
+	char	**new_env;
+	int		i;
 
-    // Look for existing variable
-    i = 0;
-    while (shell->env[i])
-    {
-        if (ft_strncmp(shell->env[i], var_name, var_name_len) == 0 &&
-            shell->env[i][var_name_len] == '=')
-        {
-            // Replace existing variable
-            free(shell->env[i]);
-            shell->env[i] = ft_strdup(var_assignment);
-            free(var_name);
-            return (shell->env[i] != NULL);
-        }
-        i++;
-    }
+	new_env = malloc(sizeof(char *) * (env_count + 2));
+	if (!new_env)
+		return (0);
+	i = 0;
+	while (i < env_count)
+	{
+		new_env[i] = shell->env[i];
+		i++;
+	}
+	new_env[env_count] = ft_strdup(var_assignment);
+	new_env[env_count + 1] = NULL;
+	if (!new_env[env_count])
+	{
+		free(new_env);
+		return (0);
+	}
+	free(shell->env);
+	shell->env = new_env;
+	return (1);
+}
 
-    // Add new variable
-    env_count = i;
-    new_env = malloc(sizeof(char*) * (env_count + 2));
-    if (!new_env)
-    {
-        free(var_name);
-        return 0;
-    }
+static int	count_env_vars(char **env)
+{
+	int	count;
 
-    // Copy existing environment
-    i = 0;
-    while (i < env_count)
-    {
-        new_env[i] = shell->env[i];
-        i++;
-    }
+	count = 0;
+	while (env[count])
+		count++;
+	return (count);
+}
 
-    // Add new variable
-    new_env[env_count] = ft_strdup(var_assignment);
-    new_env[env_count + 1] = NULL;
+int	set_env_var(t_shell *shell, char *var_assignment)
+{
+	char	*var_name;
+	char	*equals_pos;
+	int		var_name_len;
+	int		existing_index;
 
-    if (!new_env[env_count])
-    {
-        free(new_env);
-        free(var_name);
-        return 0;
-    }
-
-    // Replace old environment
-    free(shell->env);
-    shell->env = new_env;
-    free(var_name);
-
-    return 1;
+	if (!shell || !var_assignment)
+		return (0);
+	equals_pos = ft_strchr(var_assignment, '=');
+	if (!equals_pos)
+		return (0);
+	var_name_len = equals_pos - var_assignment;
+	var_name = ft_strndup(var_assignment, var_name_len);
+	if (!var_name)
+		return (0);
+	existing_index = find_env_var(shell->env, var_name, var_name_len);
+	if (existing_index != -1)
+	{
+		free(var_name);
+		return (replace_env_var(shell, existing_index, var_assignment));
+	}
+	free(var_name);
+	return (add_new_env_var(shell, var_assignment, count_env_vars(shell->env)));
 }
