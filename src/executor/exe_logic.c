1c80cd9878ffd7ef8394f9a15e79c3306d4ab7ed
David Mendoza Fernandez <dmendoza@car10s6.42barcelona.com>
2025-11-07 10:35:25 +0100

    executor norminete

--- src/executor/exe_logic.c
+++ src/executor/exe_logic.c
@@ -6,7 +6,7 @@
 /*   By: dmendoza <dmendoza@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 16:45:15 by dmendoza          #+#    #+#             */
-/*   Updated: 2025/10/26 13:39:10 by dmendoza         ###   ########.fr       */
+/*   Updated: 2025/11/07 10:33:25 by dmendoza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,57 @@ static t_command	*find_pipeline_end(t_command *start)
 	return (end);
 }
 
-/*static int	should_stop_execution(t_command *exed_cmd, int exit_status)
+static int	should_execute_next(int exit_status, t_cmd_logic logic)
 {
-	if (!exed_cmd || !exed_cmd->next)
-		return (1);
-	if (exed_cmd->logic == CMD_AND && exit_status != EXIT_SUCCESS)
-		return (1);
-	if (exed_cmd->logic == CMD_OR && exit_status == EXIT_SUCCESS)
-		return (1);
-	return (0);
-}*/
+	if (logic == CMD_AND)
+		return (exit_status == EXIT_SUCCESS);
+	else if (logic == CMD_OR)
+		return (exit_status != EXIT_SUCCESS);
+	return (1);
+}
 
+static int	execute_command_block(t_command *current, t_shell *shell,
+				t_command **next)
+{
+	t_command	*pipeline_end;
+	int			exit_status;
+
+	if (current->logic == CMD_PIPE)
+	{
+		pipeline_end = find_pipeline_end(current);
+		exit_status = execute_pipeline(current, shell);
+		*next = pipeline_end->next;
+	}
+	else
+	{
+		exit_status = execute_single_command(current, shell);
+		*next = current->next;
+	}
+	return (exit_status);
+}
+
+int	execute_logical_sequence(t_command *cmd_list, t_shell *shell)
+{
+	t_command	*current;
+	t_command	*next;
+	int			exit_status;
+	int			should_exe;
+
+	current = cmd_list;
+	exit_status = EXIT_SUCCESS;
+	should_exe = 1;
+	while (current)
+	{
+		if (should_exe)
+			exit_status = execute_command_block(current, shell, &next);
+		else
+			next = current->next;
+		should_exe = should_execute_next(exit_status, current->logic);
+		current = next;
+	}
+	return (exit_status);
+}
+/*
 int	execute_logical_sequence(t_command *cmd_list, t_shell *shell)
 {
 	t_command	*current;
@@ -66,4 +106,4 @@ int	execute_logical_sequence(t_command *cmd_list, t_shell *shell)
 		current = current->next;
 	}
 	return (exit_status);
-}
+}*/
