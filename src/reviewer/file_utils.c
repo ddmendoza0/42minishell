ddb6ba9b47f5aeabac62ff6f8ee5af38bf79eacd
bossmaya6 <dmaya-vi@car10s4.42barcelona.com>
2025-11-07 10:43:50 +0100

    norminette

--- src/reviewer/file_utils.c
+++ src/reviewer/file_utils.c
@@ -12,41 +12,6 @@
 
 #include "minishell.h"
 
-/*static char	*get_input_path(t_command *cmd)
-{
-	if (cmd->input_redir->expanded_path)
-		return (cmd->input_redir->expanded_path);
-	return (cmd->input_redir->original_token->value);
-}
-
-char	*get_input_file(t_command *cmd)
-{
-	if (!cmd || !cmd->input_redir)
-		return (NULL);
-	return (ft_strdup(get_input_path(cmd)));
-}
-
-static char	*get_output_path(t_command *cmd)
-{
-	if (cmd->output_redir->expanded_path)
-		return (cmd->output_redir->expanded_path);
-	return (cmd->output_redir->original_token->value);
-}
-
-char	*get_output_file(t_command *cmd)
-{
-	if (!cmd || !cmd->output_redir)
-		return (NULL);
-	return (ft_strdup(get_output_path(cmd)));
-}
-
-int	is_append_mode(t_command *cmd)
-{
-	if (!cmd || !cmd->output_redir)
-		return (0);
-	return (cmd->output_redir->append_mode);
-}*/
-
 static char	*get_input_path(t_redir_file *input_redir)
 {
 	if (input_redir->expanded_path)
--- src/reviewer/mode_checkers.c
+++ src/reviewer/mode_checkers.c
@@ -12,13 +12,6 @@
 
 #include "minishell.h"
 
-/*int	is_heredoc_mode(t_command *cmd)
-{
-	if (!cmd || !cmd->input_redir)
-		return (0);
-	return (cmd->input_redir->is_heredoc);
-}*/
-
 int	is_heredoc_mode(t_command *cmd)
 {
 	t_redir_file	*input;
