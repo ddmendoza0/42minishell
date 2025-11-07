d431bd8258a414ea9121d67090abde16f2a15a1f
David Mendoza Fernandez <dmendoza@car10s6.42barcelona.com>
2025-11-07 10:40:15 +0100

    removed unused code

--- src/core/shell_state.c
+++ src/core/shell_state.c
@@ -6,7 +6,7 @@
 /*   By: dmaya-vi <dmaya-vi@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/09 15:30:07 by dmaya-vi          #+#    #+#             */
-/*   Updated: 2025/10/03 19:51:03 by dmendoza         ###   ########.fr       */
+/*   Updated: 2025/11/07 10:39:57 by dmendoza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,46 +121,3 @@ int	init_shell(t_shell *shell, char **envp)
 		return (0);
 	return (1);
 }
-/*
-int	init_shell(t_shell *shell, char **envp)
-{
-	char	*pwd_str;
-	char	*current_dir;
-
-	if (!copy_environment(shell, envp))
-	{
-		perror("minishell: malloc");
-		return (0);
-	}
-	current_dir = getcwd(NULL, 0);
-	if (!current_dir)
-	{
-		perror("minishell: getcwd");
-		cleanup_shell(shell);
-		return (0);
-	}
-	shell->cwd = current_dir;
-	pwd_str = ft_strjoin("PWD=", current_dir);
-	if (!pwd_str)
-	{
-		cleanup_shell(shell);
-		return (0);
-	}
-	if (!set_env_var(shell, pwd_str))
-	{
-		free(pwd_str);
-		cleanup_shell(shell);
-		return (0);
-	}
-	free(pwd_str);
-	shell->last_exit_status = 0;
-	shell->stdin_backup = dup(STDIN_FILENO);
-	shell->stdout_backup = dup(STDOUT_FILENO);
-	if (shell->stdin_backup == -1 || shell->stdout_backup == -1)
-	{
-		perror("minishell: dup");
-		cleanup_shell(shell);
-		return (0);
-	}
-	return (1);
-}*/
