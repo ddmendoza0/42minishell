363542bf0272b28ca5e2c4a28a331cca0e19e40b
ddmendoza0 <ddmendozafernandez@gmail.com>
2025-11-11 12:05:03 +0100

    builtin exit - repairs

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -30,50 +30,72 @@ static int	is_numeric_arg(const char *str)
     return (1);
 }
 
-static int	parse_exit_code(const char *str)
+static int	check_overflow(long long num, int digit, int sign)
 {
-    long long	num;
-    int			sign;
-    int			i;
+	if (sign == 1)
+	{
+		if (num > (LLONG_MAX - digit) / 10)
+			return (1);
+	}
+	else
+	{
+		if (num > (LLONG_MAX - digit) / 10)
+			return (1);
+	}
+	return (0);
+}
 
-    num = 0;
-    sign = 1;
-    i = 0;
-    if (str[i] == '-')
-        sign = -1;
-    if (str[i] == '+' || str[i] == '-')
-        i++;
-    while (str[i])
-    {
-        num = num * 10 + (str[i] - '0');
-        i++;
-    }
-    num *= sign;
-    return ((num % 256 + 256) % 256);
+static int	parse_exit_code(const char *str, int *overflow)
+{
+	long long	num;
+	int			sign;
+	int			i;
+
+	num = 0;
+	sign = 1;
+	i = 0;
+	*overflow = 0;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i])
+	{
+		if (check_overflow(num, str[i] - '0', sign))
+		{
+			*overflow = 1;
+			return (2);
+		}
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	num *= sign;
+	return ((num % 256 + 256) % 256);
 }
 
 int	builtin_exit(char **argv, t_shell *shell)
 {
-    int	exit_code;
+	int	exit_code;
+	int	overflow;
 
-    ft_putstr_fd("exit\n", STDOUT_FILENO);
-    if (!argv[1])
-    {
-        exit_code = shell->last_exit_status;
-        exit(exit_code);
-    }
-    if (!is_numeric_arg(argv[1]))
-    {
-        ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-        ft_putstr_fd(argv[1], STDERR_FILENO);
-        ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-        exit(2);
-    }
-    if (argv[2])
-    {
-        ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
-        return (set_exit_status(shell, 1));
-    }
-    exit_code = parse_exit_code(argv[1]);
-    exit(exit_code);
+	ft_putstr_fd("exit\n", STDOUT_FILENO);
+	if (!argv[1])
+		exit(shell->last_exit_status);
+	if (!is_numeric_arg(argv[1]))
+	{
+		exit_error(shell, argv[1]);
+		exit(EXIT_MISUSE);
+	}
+	if (argv[2])
+	{
+		print_error("minishell: exit", NULL, "too many arguments");
+		return (set_exit_status(shell, EXIT_FAILURE));
+	}
+	exit_code = parse_exit_code(argv[1], &overflow);
+	if (overflow)
+	{
+		exit_error(shell, argv[1]);
+		exit(EXIT_MISUSE);
+	}
+	exit(exit_code);
 }
