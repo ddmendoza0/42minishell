be635a3e1f4c33948e34483be907f0e154d34a32
ddmendoza0 <ddmendozafernandez@gmail.com>
2025-11-11 15:19:11 +0100

    pipe - repairs

--- src/executor/exe_pipe_child.c
+++ src/executor/exe_pipe_child.c
@@ -13,7 +13,42 @@
 #include "minishell.h"
 #include <sys/wait.h>
 
-static int	execute_pipeline_command(t_command *cmd, t_shell *shell)
+static void	execute_external_direct(char **argv, t_shell *shell)
+{
+	char			*executable_path;
+	struct stat		path_stat;
+
+	executable_path = find_executable(argv[0], shell);
+	if (!executable_path)
+	{
+		if (ft_strchr(argv[0], '/'))
+			handle_error(shell, ERR_NO_SUCH_FILE, argv[0]);
+		else
+			handle_error(shell, ERR_COMMAND_NOT_FOUND, argv[0]);
+		exit(EXIT_COMMAND_NOT_FOUND);
+	}
+	if (stat(executable_path, &path_stat) == 0)
+	{
+		if (S_ISDIR(path_stat.st_mode))
+		{
+			handle_error(shell, ERR_IS_DIRECTORY, argv[0]);
+			free(executable_path);
+			exit(EXIT_CANNOT_EXECUTE);
+		}
+	}
+	if (access(executable_path, X_OK) != 0)
+	{
+		handle_error(shell, ERR_PERMISSION_DENIED, argv[0]);
+		free(executable_path);
+		exit(EXIT_CANNOT_EXECUTE);
+	}
+	execve(executable_path, argv, shell->env);
+	handle_system_error(shell, argv[0]);
+	free(executable_path);
+	exit(EXIT_CANNOT_EXECUTE);
+}
+
+static void	execute_pipeline_command(t_command *cmd, t_shell *shell)
 {
 	char	**argv;
 	int		exit_status;
@@ -29,11 +64,13 @@ static int	execute_pipeline_command(t_command *cmd, t_shell *shell)
 	}
 	setup_signals_default();
 	if (is_builtin(argv[0]))
+	{
 		exit_status = execute_builtin(argv, cmd, shell);
+		free_argv(argv);
+		exit(exit_status);
+	}
 	else
-		exit_status = execute_external(argv, shell);
-	free_argv(argv);
-	exit(exit_status);
+		execute_external_direct(argv, shell);
 }
 
 static void	setup_heredoc_if_first(t_command *current, int is_first)
@@ -58,7 +95,7 @@ void	exe_in_child(t_command *current, t_child_ctx *ctx, t_shell *shell)
 	setup_signals_default();
 	setup_heredoc_if_first(current, ctx->is_first);
 	if (!validate_command_redirections(current, shell))
-		exit (1);
+		exit(1);
 	setup_pipeline_redir(current, ctx->prev_pipe_read, ctx->pipe_fd);
 	execute_pipeline_command(current, shell);
 }
